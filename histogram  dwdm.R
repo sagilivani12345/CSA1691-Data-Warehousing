52de60b43e667aee901ac01752d350bc3f65d2ef
sagilivani12345 <112468921+sagilivani12345@users.noreply.github.com>
2023-05-03 11:42:37 +0530

    Add files via upload

---
+++ histogram  dwdm.R
@@ -0,0 +1,17 @@
+emp.date<-data.frame
+age1=c("5-6","9-10")
+a=c(10,20,12)
+b=c(19,12,13)
+c=c(4,45,14)
+photo1=data.frame(age1,a,b,c)
+photo1
+s1=cov(a,b)
+s2
+s2=cov(photo1$a.photo1$b)
+s2
+s3=cov(photo1)
+s3
+c1=c(1,1,5,5,5,5,5,8,8,10,10,10,12,14,14,14,15,15,15,15,18,18,18,18)
+hist(c1)
+
+
